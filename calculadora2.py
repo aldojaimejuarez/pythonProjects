efecb93a49345a70022685fd0b4fb8c85a525cc8
Aldo Jaime Juarez <jaimejuarez.5@MTY.MX.TELEPERFORMANCE.COM>
2021-08-29 22:52:55 -0500

    calculadora mejorada terminada

--- calculadora2.py
+++ calculadora2.py
@@ -1,1 +1,54 @@
-print(input('Bienvenido a la calculadora, ingresa el primer número: '))
+primerNumero = input('Bienvenido a la calculadora, ingresa el primer número: ')
+
+try:
+    primerNumero = int(primerNumero)
+except:
+        primerNumero = 'invalido'
+
+while primerNumero == 'invalido':
+    primerNumero = input('El dato es incorrecto, ingresa un número: ')
+    try:
+        primerNumero = int(primerNumero)
+    except:
+        primerNumero = 'invalido'
+
+else:
+    operacion = input('Ingresa la operacion que quieres realizar (+, -, *, /): ')
+
+operaciones = ['+','-','*','/']
+
+while operaciones.count(operacion) == 0:
+    operacion = input('El dato ingreso es incorrecto, ingresa uno de los siguientes símbolos  suma(+), resta(-), multiplicacion(*), división(/): ')
+
+else:
+    if operacion == '+':
+        segundoNumero = input('Elegiste suma, ingresa el segundo número: ')
+    elif operacion == '-':
+        segundoNumero = input('Elegiste resta, ingresa el segundo número: ')
+    elif operacion == '*':
+        segundoNumero = input('Elegiste multiplicación, ingresa el segundo número: ')
+    elif operacion == '/':
+        segundoNumero = input('Elegiste división, ingresa el segundo número: ')
+
+try:
+    segundoNumero = int(segundoNumero)
+except:
+    segundoNumero = 'invalido'
+
+while segundoNumero == 'invalido':
+    segundoNumero = input('El dato ingresado es incorrecto, ingresa un número: ')
+    try:
+        segundoNumero = int(segundoNumero)
+    except:
+        segundoNumero = 'invalido'
+
+else:
+    if operacion == '+':
+        print('El resultado de ', primerNumero, '+', segundoNumero, 'es:', primerNumero+segundoNumero)
+    elif operacion == '-':
+        print('El resultado de ', primerNumero, '-', segundoNumero, 'es:', primerNumero-segundoNumero)
+    elif operacion == '*':
+        print('El resultado de ', primerNumero, '*', segundoNumero, 'es:', primerNumero*segundoNumero)
+    elif operacion == '/':
+        print('El resultado de ', primerNumero, '/', segundoNumero, 'es:', primerNumero/segundoNumero)
+
