d4183343565842a20793dca6e781dd70846c1c80
aldojaimejuarez <aldojaimejuarez@gmail.com>
2021-11-03 11:20:50 -0600

    exercises 1,2,3

---
+++ exercise3.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  4 2021
+@author: me@aldojaimejuarez.com
+"""
+
+"""
+Descripción: Desarrollar un algoritmo que sea capaz de identificar dentro de la matriz, que elemento es
+una fruta y cual no.
+fruits = ['apple', 'banana', 'meat', 'bread']
+"""
+
+def fruitOrNot():
+
+    fruits = ['apple', 'banana', 'meat', 'bread']
+
+    element=input('Select and enter the name of the element that want to know is or not a fruit: apple, banana, meat and bread: ')
+
+    while element not in fruits:
+        element=input('You enter a invalid element, try again: apple, banana, meat and bread: ')
+
+    else:
+        if fruits.index(element) == 0 or fruits.index(element) == 1:
+            print('The element IS a fruit')
+
+        elif fruits.index(element) == 2 or fruits.index(element) == 3:
+            print('The element IS NOT a fruit')
+
+
+if __name__ == '__main__':
+    fruitOrNot()
+
